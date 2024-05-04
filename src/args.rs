e84106b035499fb7c3f11b4800a5392b37feb157
t-ski <t.schiepanski@gmx.de>
2024-05-04 20:25:51 +0200

    Add CLI argument parsing module

---
+++ src/args.rs
@@ -0,0 +1,44 @@
+fn get_raw() -> Vec<String> {
+    return std::env::args().collect();
+}
+
+fn get_index(name: &str, shorthand: Option<&str>) -> Option<usize> {
+    let args = get_raw();
+
+    let index = args.iter().position(|s| *s.to_lowercase() == format!("--{}", name.to_lowercase()));
+    let shorthand_index = args.iter().position(|s| *s.to_lowercase() == format!("-{}", shorthand.unwrap_or("").to_lowercase()));
+    
+    return match index != None || shorthand_index != None {
+        true => Some(std::cmp::min(index.unwrap_or(args.len()), shorthand_index.unwrap_or(args.len()))),
+        false => None
+    };
+}
+
+
+pub fn parse_positional(index: usize) -> Option<String> {
+    let args: Vec<String> = get_raw();
+    if index >= args.len() - 1 {
+        return None;
+    }
+    return Some(args[index + 1].to_string());
+}
+
+pub fn parse_flag(name: &str, shorthand: Option<&str>) -> bool {
+    if get_index(name, shorthand) == None {
+        return false;
+    }
+    return true;
+}
+
+pub fn parse_option(name: &str, shorthand: Option<&str>) -> Option<String> {
+    let index: Option<usize> = get_index(name, shorthand);
+    if index == None {
+        return None;
+    }
+    let args: Vec<String> = get_raw();
+    let option_index: usize = index.unwrap() + 1;
+    if option_index >= args.len() {
+        return None;
+    }
+    return Some(args[option_index].to_string());
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+mod args;
+mod diff;
+
+
 fn main() {
-    println!("Hello, world!");
+    println!("positional: {}", args::parse_positional(1).unwrap());
+    println!("flag: {}", args::parse_flag("del", Some("D")));
+    println!("option: {}", args::parse_option("f", Some("F")).unwrap());
+
+    println!("lcs: {}", diff::longest_common_subsequence(String::from("abcdef"), String::from("zabycdxwfd")));
 }
